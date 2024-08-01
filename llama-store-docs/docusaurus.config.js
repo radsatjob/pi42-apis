e69705585d82267c07d0c390140174cdb2ddefa8
radsatjob <radhikatk@gmail.com>
2024-08-01 19:43:00 +0530

    commit

--- llama-store-docs/docusaurus.config.js
+++ llama-store-docs/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://radsatjob.pi42-apis.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'liblab', // Usually your GitHub org/user name.
-  projectName: 'llama-store', // Usually your repo name.
+  organizationName: 'radsatjob', // Usually your GitHub org/user name.
+  projectName: 'pi42-apis', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
